3846a441ddeb175408dbf71af1b6317c8feea7e5
John Kang <hbkang02@gmail.com>
2022-12-08 22:46:54 -0800

    linted product finalized

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,9 +1,4 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- * 
- */
+
 const escape = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
@@ -24,6 +19,12 @@ let renderTweets = function (tweets) {
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
 }
+
+const getTweets = () => $.ajax('/tweets',
+  { method: 'GET' })
+  .then((data) => {
+    renderTweets(data);
+  })
 
 let createTweetElement = function (tweet) {
   let timePosted = timeago.format(tweet.created_at);
@@ -57,13 +58,6 @@ let createTweetElement = function (tweet) {
 }
 
 
-const getTweets = () => $.ajax('/tweets',
-  { method: 'GET' })
-  .then((data) => {
-    renderTweets(data);
-  })
-
-
 $(document).ready(function () {
   $(".alert-container").hide()
   getTweets()
@@ -71,27 +65,30 @@ $(document).ready(function () {
   $(".form-container").submit((event) => {
     event.preventDefault()
 
-    console.log($)
     const $textArea = $(this).find('.textInput');
     const inputText = $textArea.val().trim();
 
     if (!inputText) {
+
       $(".alert-message").text("No message was typed in. Please type in your message.")
       $(".alert-container").show()
       return
+
     } else if (inputText.length > 140) {
       $(".alert-message").text('Messages cannot go over 140 characters.')
       $(".alert-container").show()
       return
     }
+
     $(".alert-container").hide()
+
     $.ajax('/tweets', {
       data: { text: inputText },
       method: 'POST',
       success: () => {
         getTweets();
-        // $textArea.val(''); // clear textarea
-        // $('.counter').text('140'); // reset counter to 140
+        $textArea.val(''); // clear textarea
+        $('.counter').text('140'); // reset counter to 140
       },
     })
   })
